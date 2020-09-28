49e31be9b237d9c879628343651ad602e693743d
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:30:50 +0200

    actions type / creators creation

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+// ACTIONS TYPES
+export const FETCH_TEXT_HOME = 'FETCH_TEXT_HOME';
+export const FETCH_TEXT_CONTACT = 'FETCH_TEXT_CONTACT';
+export const FETCH_LANG = 'FETCH_LANG';
+export const CHANGE_LANG = 'CHANGE_LANG';
+
+//ACTIONS CREATORS
+
+export const fetchTextHome = () => ({
+  type: FETCH_TEXT_HOME,
+})
+
+export const fetchTextContact = () => ({
+  type: FETCH_TEXT_CONTACT,
+})
+
+export const fetchLang = () => ({
+  type: FETCH_LANG,
+})
+
+export const changeLang = (lang) => ({
+  type: CHANGE_LANG,
+  lang,
+})
