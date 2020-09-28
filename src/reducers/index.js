faa225cc7d6a093c2fc9362855730ec0ef415b04
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:28:19 +0200

    reducer creation

---
+++ src/reducers/index.js
@@ -0,0 +1,59 @@
+import { 
+  CHANGE_LANG, 
+  FETCH_TEXT_HOME,
+  FETCH_TEXT_CONTACT,
+  FETCH_LANG,
+} from '../actions/index';
+
+const initialState = {
+  homeMessage: {
+    'fr': 'Accueil',
+    'en': 'Home',
+  },
+  contactMessage: {
+    'fr': 'Contact',
+    'en': 'Contact',
+  },
+  languages: [
+    { id: 1, code: 'en', name: 'English' },
+    { id: 2, code: 'fr', name: 'Français' }
+  ],
+  selectedLanguage: "Français",
+};
+
+/**
+* @function reducer
+* @param {state} state courant ou a default un objet comme state = initalState
+* @param {action} action en fonction de cette action, on va pouvoir changer le state
+* @return toujours un state
+* Switch permet de changer le state en fonction du type d'actions
+*/
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case Fetch text props
+    case FETCH_TEXT_HOME: 
+      return {
+        ...state,
+        homeMessage: action.message 
+      }
+    case FETCH_TEXT_CONTACT: 
+      return {
+        ...state,
+        contactMessage: action.message 
+      }
+    // case Fetch lang props
+    case FETCH_LANG: 
+      return {
+        ...state,
+        languages: action.lang
+      }
+    // case CHANGE lang props
+    case CHANGE_LANG:
+      return {
+        ...state,
+         selectedLanguage: action.lang
+        };
+    default: return state;
+  }
+};
+export default reducer;
