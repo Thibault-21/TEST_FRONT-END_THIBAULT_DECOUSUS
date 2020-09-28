4f5f45dd005d2d2e836bcd24d9ca471d9e4b81be
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:30:02 +0200

    navbar container creation

---
+++ src/container/navBar/navbar.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { changeLang } from '../../actions/index'
+import NavBar from '../../components/NavBar/NavBar';
+
+const mapStateToProps = (state) => { 
+  // console.log(state.languages)
+  return ({
+    languages: state.languages,
+    titles: state.title
+  });
+}
+const mapDispatchToProps = (dispatch) => ({
+  selectedOnLanguage: (lang) => {
+    dispatch(changeLang(lang))
+  }
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
