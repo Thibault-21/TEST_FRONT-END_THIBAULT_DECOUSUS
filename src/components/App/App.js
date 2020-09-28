351a9d48a15bab7b0cb3c9f4543b9db5886e3591
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:29:50 +0200

    navbar container import path updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 // import PropTypes from 'prop-types';
-import NavBar from '../../container/NavBar/index';
+import NavBar from '../../container/NavBar/navbar';
 import Page from '../../container/page/page';
 
 import './App.scss';
