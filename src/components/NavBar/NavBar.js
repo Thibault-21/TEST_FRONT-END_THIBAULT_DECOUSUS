e0f343bcde70513c664964e93e994bb696f9ba99
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:27:10 +0200

    NavBar creation + props

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NavBar = ({
+  languages,
+  // titles,
+  selectedOnLanguage 
+}) => {
+  console.log('languages', languages)
+  // console.log('languagesLang', languagesLang)
+  console.log('languagesSelected', selectedOnLanguage)
+
+  return (
+    <Navbar bg="primary" variant="dark">        
+        <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/home"> Accueil </Nav.Link>    
+        <Nav.Link as={Link} to="/contact"> Contact </Nav.Link>    
+          <NavDropdown title="Lang" id="nav-dropdown"> 
+            {languages.map((lang) => {
+              return (
+                <NavDropdown.Item
+                  key={lang.id}
+                  eventKey={lang.id}
+                  onSelect={() => selectedOnLanguage(lang.name)}
+                  exact
+                >
+                  {lang.name}
+                </NavDropdown.Item>
+            )})}
+          </NavDropdown>            
+        </Nav>
+    </Navbar>
+  );
+}
+NavBar.propTypes = {
+  languages: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+  ).isRequired,
+  selectedOnLanguage: PropTypes.func.isRequired,
+};
+
+export default NavBar;
