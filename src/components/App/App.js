edda99eebc4ca294ba663d3f328ad2c2e597c052
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-28 21:26:19 +0200

    App component set : Navbar + Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+import NavBar from '../../container/NavBar/index';
+import Page from '../../container/page/page';
+
+import './App.scss';
+
+// useEffect(translation, [])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <NavBar />
+      <Page />
     </div>
   );
 }
+App.propTypes = {
+
+};
 
 export default App;
