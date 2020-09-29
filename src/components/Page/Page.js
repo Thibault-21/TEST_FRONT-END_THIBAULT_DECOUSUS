bbdfea59b29dbc47322430937e74dc674ca166a6
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-29 19:51:42 +0200

    optimization  Page (conditional rendering)

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -3,32 +3,32 @@ import { Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Contact from '../Contact/Contact';
 
-const Page = ({ selectedLanguage, homeMessage, contactMessage }) => {
-  
-  console.log('page component', homeMessage, contactMessage)
-  
-  return (
-  <div className='page'>
-     {selectedLanguage === "Français" && (
-        <>
-          <Route path="/home" exact>
-            <Home text={homeMessage.fr} />
-          </Route>
-          <Route path="/contact" exact >
-            <Contact text={contactMessage.fr} />
-          </Route>
-        </>
-      )}
-      {selectedLanguage === "English" && (
-        <>
-          <Route path="/home" exact>
-            <Home text={homeMessage.en} />
-          </Route>
-          <Route path="/contact" exact >
-            <Contact text={contactMessage.en} />
-          </Route>
-        </>
-      )}  
-  </div>
-)};
+const Page = ({ 
+  selectedLanguage, 
+  homeMessage, 
+  contactMessage 
+}) => (
+    <div className='page'>
+       {selectedLanguage === "Français" 
+        ?
+          <>
+            <Route path="/home" exact>
+              <Home text={homeMessage.fr} />
+            </Route>
+            <Route path="/contact" exact >
+              <Contact text={contactMessage.fr} />
+            </Route>
+          </>
+        :
+          <>
+            <Route path="/home" exact>
+              <Home text={homeMessage.en} />
+            </Route>
+            <Route path="/contact" exact >
+              <Contact text={contactMessage.en} />
+            </Route>
+          </>
+        }
+    </div>
+  );
 export default Page;
