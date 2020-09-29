255ec3a7c203b7ccb6a5ccdae5a8ad558702d18d
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-29 19:52:15 +0200

    optimization  page container

--- src/container/page/page.js
+++ src/container/page/page.js
@@ -2,14 +2,12 @@ import { connect } from 'react-redux';
 
 import Page from '../../components/Page/Page';
 
-const mapStateToProps = (state) => {
-  // console.log('pageContainer', state.homeMessage, state.contactMessage) 
-  return ({
+const mapStateToProps = (state) => ({
     selectedLanguage: state.selectedLanguage, 
     homeMessage: state.homeMessage,
     contactMessage: state.contactMessage,
   });
-}
+
 const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Page);
