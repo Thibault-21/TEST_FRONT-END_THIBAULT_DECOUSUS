c44719b27b764ed9b983b7d1eb363481eb70fc89
Thibault-21 <thib.dec.pro@gmail.com>
2020-09-29 19:51:55 +0200

    optimization  navbar container

--- src/container/navBar/navbar.js
+++ src/container/navBar/navbar.js
@@ -2,18 +2,15 @@ import { connect } from 'react-redux';
 import { changeLang } from '../../actions/index'
 import NavBar from '../../components/NavBar/NavBar';
 
-const mapStateToProps = (state) => { 
-  // console.log(state.languages)
-  return ({
+const mapStateToProps = (state) => ({
     languages: state.languages,
     titles: state.title
   });
-}
+
 const mapDispatchToProps = (dispatch) => ({
   selectedOnLanguage: (lang) => {
     dispatch(changeLang(lang))
   }
-
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
